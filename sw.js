03b75ff8b136c55827d104b88b8aa7d7fa03c59d
ymaooiii-dev <ymaooiii@gmail.com>
2026-01-12 08:47:23 +0800

    refactor: Clean up PWA manifest and SW
    Removes unnecessary scope from manifest and simplifies the service worker's activation logic to unregister and refresh clients. Updates import maps to use a more direct react-dom client import.

--- sw.js
+++ sw.js
@@ -1,18 +1,8 @@
-// Suicide Service Worker - 清理所有缓存并自毁
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((k) => caches.delete(k)));
-    }).then(() => {
-      return self.registration.unregister();
-    }).then(() => {
-      return self.clients.matchAll();
-    }).then((clients) => {
-      clients.forEach((client) => client.navigate(client.url));
-    })
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    self.registration.unregister()
+      .then(() => self.clients.matchAll())
+      .then(clients => clients.forEach(c => c.navigate(c.url)))
   );
 });
